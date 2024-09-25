f97bc584b9435398fcae20fef2a43c50a00d4672
Zafar <su10409@studenti.polito.it>
2024-09-25 12:00:48 +0500

    removed packega

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { createApp } from 'vue'
 import i18n from './i18n'
 import { createVuestic } from 'vuestic-ui'
-import ElementPlus from "element-plus";
 import { createGtm } from '@gtm-support/vue-gtm'
 import { createPinia } from 'pinia'
 
 import stores from './stores'
 import router from './router'
 import vuesticGlobalConfig from './services/vuestic-ui/global-config'
-import "element-plus/dist/index.css";
-import ruRu from "element-plus/dist/locale/ru.mjs";
 import "@fortawesome/fontawesome-free/css/all.css";
 import App from './App.vue'
 
@@ -20,9 +17,6 @@ app.use(router)
 app.use(i18n)
 app.use(createVuestic({ config: vuesticGlobalConfig }))
 app.use(createPinia())
-app.use(ElementPlus, {
-  locale: ruRu,
-});
 
 if (import.meta.env.VITE_APP_GTM_ENABLED) {
   app.use(
